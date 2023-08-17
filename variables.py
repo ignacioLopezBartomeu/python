6449fe4fce81e1722b39e87f420602ba07961cb1
ignacioLopezBartomeu <a01422764@tec.mx>
2023-08-17 11:23:03 -0600

    Variables V1

---
+++ variables.py
@@ -0,0 +1,16 @@
+year = 2020
+sentence = "my name is adsoft"
+sarah, bob, mike = 16,20,25
+name, age = "mary", 24
+mary = fer = 20
+PI = 3.1416
+e = 2.17
+
+print ("year =", year)
+print (sentence)
+print ("\n sarah =", sarah, "\n\t bob =", bob, "\n mike = ", mike)
+print ("\n\t name =", name)
+print ("\n\t age=", age)
+print  (mary, fer)
+print ("PI =", PI)
+print ("e =", e)
